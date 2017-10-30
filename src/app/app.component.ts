68293940b2d5894ead3a11c3bc084888c256ba0f
Justin Muller <wickedjust@hotmail.com>
2017-10-30 21:17:36 +1100

    Animating reply response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,36 @@ import { sonnets } from './sonnets';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  formattedSonnets = sonnets.map((sonnet) => {
-    return {
-      ...sonnet,
-      linesFormatted: sonnet.lines.join('\n')
-    }
-  });
-
-  messages = [];
 
   model = {
-    message: null
+    formattedSonnets: sonnets.map((sonnet) => {
+      return {
+        ...sonnet,
+        linesFormatted: sonnet.lines.join('\n')
+      }
+    }),
+    messages: [],
+    message: null,
+    replying: false
+  };
+
+  simulateMessageResponse = () => {
+    setTimeout(() => {
+      this.model.replying = true;
+
+
+
+    }, 2000);
   };
 
   onSubmit = (data) => {
-    this.messages.push({
+    this.model.messages.push({
       text: this.model.message
     });
 
     //after completed with message, reset field.
     this.model.message = null;
+
+    this.simulateMessageResponse();
   };
 }
